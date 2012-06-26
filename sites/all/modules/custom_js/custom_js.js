a27b54a78f033a3dbd0e05b6b26a44916bcf8ba3
Tekton Labs - Testing Server <all3k@achernar.dreamhost.com>
2012-06-26 11:13:47 -0700

    Nuevos modulos, modulo creado y modificiaciones a css

---
+++ sites/all/modules/custom_js/custom_js.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($){
+	$('#edit-field-stock-und-0-value').attr("disabled", "disabled");
+	$('#edit-field-caracteristica-und').change(function(){
+		var caracteristica = $('#edit-field-caracteristica-und').val();
+		if(caracteristica == 'multiple'){
+			$('#edit-field-stock-und-0-value').removeAttr("disabled");
+		}else{
+			$('#edit-field-stock-und-0-value').attr("disabled", "disabled");
+		}
+	});
+
+	$('#edit-field-stock-und-0-value').blur(function(){
+		var stock = $('#edit-field-stock-und-0-value').val();
+		if(stock < 1 || isNaN(stock)){
+			alert("El stock no puede ser menor a 1. Ponga un valor permitido");
+			$('#edit-field-stock-und-0-value').val(1);
+		}
+	});
+});
